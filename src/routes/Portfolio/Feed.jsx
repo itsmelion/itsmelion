ca176ccfb96c096c34708775a715a160f9f0dc36
Chris Lion <christhopherleao@icloud.com>
2018-10-21 10:32:45 -0300

    added notice

--- src/routes/Portfolio/Feed.jsx
+++ src/routes/Portfolio/Feed.jsx
@@ -5,16 +5,25 @@ import portfolio from '../../data/portfolio';
 const Feed = () => (
   <main contain="" flex="" id="Portfolio-feed">
     <header className="mv2">
-      <h1>
+      <h2 className="mt2">
         Showcase
         <span style={{ marginLeft: '-5px' }} className="beta-tag">
           beta
         </span>
-      </h1>
+      </h2>
     </header>
 
     <section row="">
       {portfolio.map(work => <Work work={work} />)}
+    </section>
+
+    <section className="alert-info p1 mv1">
+      <h3>Notice:</h3>
+      <p>
+        this section is under development, and there is more content to be submitted.
+        <br />
+        Check back in a while.
+      </p>
     </section>
   </main>
 );
