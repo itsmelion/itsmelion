3f58a58d17e256fdcc0f41c50f85a4ab43ccb3a8
Chris Lion <christhopherleao@icloud.com>
2018-10-25 11:51:43 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ const yearsFromNow = (y = 2016, m = 0) => {
   if (month >= 6) { year += 1; }
 
   return Math.ceil((new Date() - new Date(year, month, 0)) / 1000 / 60 / 60 / 24 / 365);
-}
+};
 
 window.lang = getLanguage() || process.env.REACT_APP_DEFAULT_LANG;
 window.yearsFromNow = yearsFromNow;
