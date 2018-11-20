ff4fa020db6e483d7011be4cfc99c0ed99bd074f
Chris Lion <christhopherleao@icloud.com>
2018-11-20 00:19:51 -0200

    added certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -4,19 +4,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCertificate,
   faChevronDown,
-  faEye,
 } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
 const certifications = [
   {
-    ref: '',
-    name: 'Advanced concepts of JavaScript',
+    ref: 'https://www.udemy.com/certificate/UC-FJV8DRT2/',
+    // eslint-disable-next-line no-script-url
+    name: 'Javascript: Understanding the Weird Parts',
     provider: 'Udemy',
+    extension: '11.5 hours',
     disciplines: [
+      'Syntax Parsers, Execution Contexts, and Lexical Environments',
+      'Creation and Hoisting',
+      'The Scope Chain',
+      'Asynchronous Callbacks',
+      'Operator Precedence and Associativity',
+      'Coercion',
+      'By Value vs By Reference',
+      'Object Functions And This',
+      'Function Overloading',
+      'Immediately Invoked Functions Expressions (IIFEs)',
       'Understanding Closures',
-      'Execution contexts',
-      'Function Prototyping',
+      'Function Factories, Closures and Callbacks',
+      'call(), apply(), and bind()',
+      'Functional Programming',
+      'Classical vs Prototypal Inheritance',
+      'Reflection and Extend',
+    ],
+  },
+
+  {
+    ref: 'https://www.udemy.com/certificate/UC-AOUWOOM1/',
+    name: 'Angular 2 From The Ground Up',
+    provider: 'Udemy',
+    extension: '11.5 hours',
+    disciplines: [
+      'Concepts of Components, Templates, Services, and Dependency Injection',
+      'Working with Templates, data binding and event handling',
+      'Validating forms using the Angular Forms module',
+      'Making HTTP requests and integrating with a backend using a REST API',
+      'Setting up a production-ready build workflow using NPM and Webpack',
+      'Handling navigation in a single-page application with the Angular Router',
+      'Writing unit tests with Jasmine and running them with Karma',
+    ],
+  },
+
+  {
+    ref: 'https://www.udemy.com/certificate/UC-5EANJJIY/',
+    name: 'NodeJS & MongoDB',
+    provider: 'Udemy',
+    extension: '15.5 hours',
+    disciplines: [
+      'Integrate MySQL and MongoDB',
+      'Design Pattern MVC',
+      'Express Framework, and EJS as view engine',
+      'RESTful APIs',
+      'Body-parser: and requests parameters',
+      'Express-validator: validating your data',
+      'Session and autenthication',
+      'Cryptography MD5 with Crypto',
+      'Connect-Multiparty: uploading files',
+      'Websockets',
     ],
   },
 ];
@@ -37,19 +87,25 @@ class Certifications extends PureComponent {
 
         <div row="" align="start">
           {certifications.map(certification => (
-            <article className="item" flex="initial" key={certification.ref}>
-              <div row="nowrap">
-                <div className="cert" row="nowrap" align="center center">
+            <article className="item" flex="nogrow" key={certification.ref}>
+              <div align="between center" row="nowrap">
+                <div
+                  flex=""
+                  className="cert"
+                  row="nowrap"
+                  align="center center"
+                >
                   <FontAwesomeIcon icon={faCertificate} />
-                  <h6>
+                  <h6 flex="">
                     {certification.name}
                     <sub>provided by Udemy</sub>
                   </h6>
                 </div>
 
                 <Button
+                  flex="none"
                   title="View Certificate"
-                  theme="outline"
+                  theme="outline view"
                   link={certification.ref}
                 >
                   <FontAwesomeIcon icon={faEye} />
@@ -65,7 +121,7 @@ class Certifications extends PureComponent {
                 what was learned?
               </button>
 
-              <ul className={`materias ${open}`}>
+              <ul flex="" className={`materias ${open}`}>
                 {certification.disciplines.map(content => (
                   <li key={content}>{content}</li>
                 ))}
