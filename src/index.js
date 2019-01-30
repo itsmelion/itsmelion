c16fba1fd5af3d0ebdb5b15f7e6fe17c075b5ac9
christhopher.leao <christhopherleao@icloud.com>
2019-01-30 12:20:24 -0200

    clear service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReactGA from 'react-ga';
-import * as serviceWorker from './serviceWorker';
 import Topbar from './components/Topbar/Topbar';
 import Footer from './components/Footer/Footer';
 import ScrollToTop from './components/ScrollTop';
@@ -40,8 +39,3 @@ if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize(process.env.REACT_APP_ANALYTICS);
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
