d03a3334627e634d0dd0f18174ba63ce84e04657
Christhopher Lion <christhopherleao@icloud.com>
2019-07-11 15:40:09 +0100

    uncomment code

--- src/index.jsx
+++ src/index.jsx
@@ -27,17 +27,15 @@ render(
   <Router>
     <ScrollToTop>
       <Topbar />
-      <Splash />
-      {/* <Suspense fallback={<Splash />}>
+      <Suspense fallback={<Splash />}>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route path="/cases" component={Cases} />}
+          {/* <Route path="/cases" component={Cases} /> */}
           <Route path="/portfolio" component={Feed} />
           <Route path="/resume" component={Resume} />
           <Route path="/print" component={Print} />
         </Switch>
       </Suspense>
-      */}
       <Footer />
     </ScrollToTop>
   </Router>,
